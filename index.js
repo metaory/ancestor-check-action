99b9086bacf7b5dc283f199c86eca358e05ab63b
metaory <metaory@gmail.com>
2022-12-12 15:41:15 +0800

    updated action

--- index.js
+++ index.js
@@ -14,6 +14,20 @@ async function run() {
     await wait(parseInt(ms));
     core.info((new Date()).toTimeString());
 
+    const owner = 'metaory'
+    const repo = 'git-playground'
+    const pull_number = 1
+    const getFiles = async (page = 1, list = []) => {
+      const files = await octokit.request(`GET /repos/${owner}/${repo}/pulls/${pull_number}/files?per_page=100&page=${page}`)
+      if (!files.data.length) {
+        return list
+      } else {
+        list.push(...files.data)
+        return getFiles(page + 1, list)
+      }
+    }
+    const files = await getFiles()
+
     core.setOutput('time', new Date().toTimeString());
   } catch (error) {
     core.setFailed(error.message);
